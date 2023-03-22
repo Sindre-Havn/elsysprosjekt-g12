aec0ebeaf6f97667a0a8d24a119deb96338055a3
Sindre-Havn <89873235+Sindre-Havn@users.noreply.github.com>
2023-03-22 11:41:24 +0100

    add singlethread file

--- i2s_driver_draft.py
+++ i2s_driver_draft.py
@@ -2,30 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 frequency = 10 # Hz
-wavelength = 1/frequency * 1000
-gpio_pin = 4
+wavelength = 1/frequency
+halfwave = wavelength / 2
+gpio_pin = 18
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin,GPIO.OUT,initial=GPIO.LOW)
 
 while True:
     GPIO.output(gpio_pin,GPIO.HIGH)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.HIGH)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.HIGH)
-    time.sleep(wavelength)
+    time.sleep(halfwave)
     GPIO.output(gpio_pin,GPIO.LOW)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.LOW)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.HIGH)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.LOW)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.HIGH)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.LOW)
-    time.sleep(wavelength)
-    GPIO.output(gpio_pin,GPIO.LOW)
-    time.sleep(wavelength)
+    time.sleep(halfwave)
---
+++ singlethread_i2s_draft.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+frequency = 10 # Hz
+wavelength = 1/frequency
+halfwave = wavelength / 2
+gpio2 = 2
+gpio3 = 3
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio2,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(gpio3,GPIO.OUT,initial=GPIO.LOW)
+
+while True:
+    GPIO.output(gpio2,GPIO.HIGH)
+    time.sleep(halfwave)
+    GPIO.output(gpio3,GPIO.HIGH)
+    time.sleep(halfwave)
+    GPIO.output(gpio2,GPIO.LOW)
+    time.sleep(halfwave)
+    GPIO.output(gpio3,GPIO.LOW)
+    time.sleep(halfwave)
