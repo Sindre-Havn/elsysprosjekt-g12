df5c073f973b721264bfe7d43bde604dde42adc2
Sindre-Havn <89873235+Sindre-Havn@users.noreply.github.com>
2023-03-22 11:25:33 +0100

    edit i2s

--- i2s_driver_draft.py
+++ i2s_driver_draft.py
@@ -1,31 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
-frequency = 1000 # Hz
+frequency = 10 # Hz
 wavelength = 1/frequency * 1000
-buzzer_pin = 4
+gpio_pin = 4
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzer_pin,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(gpio_pin,GPIO.OUT,initial=GPIO.LOW)
 
 while True:
-    GPIO.output(buzzer_pin,GPIO.HIGH)
+    GPIO.output(gpio_pin,GPIO.HIGH)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.HIGH)
+    GPIO.output(gpio_pin,GPIO.HIGH)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.HIGH)
+    GPIO.output(gpio_pin,GPIO.HIGH)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.LOW)
+    GPIO.output(gpio_pin,GPIO.LOW)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.LOW)
+    GPIO.output(gpio_pin,GPIO.LOW)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.HIGH)
+    GPIO.output(gpio_pin,GPIO.HIGH)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.LOW)
+    GPIO.output(gpio_pin,GPIO.LOW)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.HIGH)
+    GPIO.output(gpio_pin,GPIO.HIGH)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.LOW)
+    GPIO.output(gpio_pin,GPIO.LOW)
     time.sleep(wavelength)
-    GPIO.output(buzzer_pin,GPIO.LOW)
+    GPIO.output(gpio_pin,GPIO.LOW)
     time.sleep(wavelength)
