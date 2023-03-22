c87c55686f6afd28458ee547325113000e82763a
Sindre-Havn <89873235+Sindre-Havn@users.noreply.github.com>
2023-03-22 12:23:13 +0100

    change to pigpio library in singelthread

--- singlethread.cpp
+++ singlethread.cpp
@@ -6,19 +6,22 @@ using namespace std;		// No need to keep using “std”
 int main()
 {
 wiringPiSetup();			// Setup the library
-pinMode(2, OUTPUT);		// Configure GPIO0 as an output
+gpioSetMode(2, PI_OUTPUT);
+gpioSetMode(3, PI_OUTPUT);
 
-frequency = 1_000_000 // Hz
-wavelength = 1/frequency // Hz
-halfwave = wavelength/2
+frequency = 1_000_000; // Hz
+wavelength = 1/frequency; // Hz
+halfwave = wavelength/2;
 
 // Main program loop
 while(1)
 {
 	// Toggle the LED
-	digitalWrite(2, 1);
+	gpioWrite(2, 1);
+	gpioWrite(3, 1);
     delay(halfwave);
-	digitalWrite(2, 0);
+	gpioWrite(2, 0);
+	gpioWrite(3, 0);
     delay(halfwave);
 }
 
