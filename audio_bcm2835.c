cac598100b8ee44bea4277169424250f0f025b85
Sindre-Havn <89873235+Sindre-Havn@users.noreply.github.com>
2023-03-27 16:34:13 +0200

    update audio_bcm

--- audio_bcm2835.c
+++ audio_bcm2835.c
@@ -6,18 +6,19 @@ int main(int argc, char *argv[]) {
     if(!bcm2835_init())
 	return 1;
 
-    int frequency     = 20000;
-    double wavelength = 1 / frequency * 1000;
-    double halfwave   = wavelength / 2;
+    int frequency      = 200000;
+    double wavelength  = 1 / frequency;
+    double halfwave    = wavelength / 2;
+    double halfwave_ms = halfwave * 1000;
 
     // Set the pin to be an output
     bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
 
     while(1) { // Blink
 	bcm2835_gpio_write(PIN, HIGH);
-	delay(halfwave);
+	delay(halfwave_ms);
 	bcm2835_gpio_write(PIN, LOW);
-	delay(halfwave);
+	delay(halfwave_ms);
     }
 
     return 0;
